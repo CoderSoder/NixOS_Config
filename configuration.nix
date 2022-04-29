0bb98e68fdc029e082dbc320789f10883793c12b
CoderSoder <83740126+CoderSoder@users.noreply.github.com>
2022-04-29 20:59:39 +0000

    Enabled SSH

--- configuration.nix
+++ configuration.nix
@@ -174,7 +174,7 @@ displayManager = {
   xdg.portal.wlr.enable = true;
 
   ## Enable the OpenSSH daemon ##
-   # services.openssh.enable = true;
+  services.openssh.enable = true;
 
 
   # Some programs need SUID wrappers, can be configured further or are started in user sessions.
